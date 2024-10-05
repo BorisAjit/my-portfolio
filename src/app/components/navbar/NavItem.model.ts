8100d2474f00d82a6337abe97b81e188d252b75f
ba371 <borisajit33@gmail.com>
2024-10-05 22:41:09 +0530

    refactor navbar and few enhancement for home page

---
+++ src/app/components/navbar/NavItem.model.ts
@@ -0,0 +1,6 @@
+export class NavItem {
+  constructor(
+    public id: string,
+    public placeholder: string
+  ) {}
+}
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { NavItem } from './NavItem.model';
 
 @Component({
   selector: 'app-navbar',
@@ -14,6 +15,13 @@ export class NavbarComponent {
 
   @Input("activePage")
   activePage = "home"
+  navList = [
+    new NavItem("home", "Home"),
+    new NavItem("about", "About"),
+    new NavItem("skills", "Skills"),
+    new NavItem("experience", "Experience"),
+    new NavItem("contact", "Contact"),
+  ]
   scrollTo(section: string) {
     document.getElementById(section)?.scrollIntoView({ behavior: 'smooth' });
     this.activePage = section
